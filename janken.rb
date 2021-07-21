3995cc37797a614787514c3630f153e36037cceb
fujitamariko <fujyta.mrk@gmail.com>
2021-07-21 09:08:09 +0900

    じゃんけんゲーム

--- janken.rb
+++ janken.rb
@@ -6,18 +6,16 @@ class Player #クラス名を定義
       puts "0:グー, 1:チョキ, 2:パー"
       # 変数「input_hand」にプレイヤーの入力値を代入します。
       # ヒント：getsメソッドについて調べてみましょう。
-      input_hand = gets.to_i ##to_iにすることで文字列を数値に変換できる
+      # chompは末尾の改行コードを削除してくれる（文字列の末尾に改行コードが含まれている場合がある為）
+      input_hand = gets.chomp
       # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
       while true
         # if 「input_hand」が「0, 1, 2」のいずれかの場合だった場合
         # ヒント：include?メソッドについて調べてみましょう。
-        if input_hand = (0..2)
-        input_hand.include?(0)
-        input_hand.include?(1)
-        input_hand.include?(2)
+        if input_hand == "0" || input_hand == "1" || input_hand == "2"
           # 「input_hand」をそのまま返す。
           # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
-          return input_hand
+          return input_hand.to_i
         # else それ以外の場合
         else
           # プレイヤーに「0〜2」を入力させる文章を表示させる。
@@ -27,6 +25,7 @@ class Player #クラス名を定義
           puts "0〜2の数字を入力してください。"
           puts "0:グー, 1:チョキ, 2:パー"
           input_hand = gets.to_i
+          return
         # end if文のend
         end
       # end whike文のend
